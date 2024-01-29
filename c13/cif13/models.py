599b6eccd5295fd184f4f2ee9aa1193ed69e95b0
Jlestat <jlestatt@gmail.com>
2024-01-29 23:20:22 +0300

    create models

--- c13/cif13/models.py
+++ c13/cif13/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(verbose_name='Название новости', max_length=255)
+    content = models.TextField(verbose_name='Содержание новости')
+    published_date = models.DateTimeField(verbose_name='Дата публикации', auto_now_add=True)
+
+    class Meta:
+        ordering = ['-published_date']
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
+    def __str__(self):
+        return self.title
